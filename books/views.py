4ac9a706e0873c4e5d7ce24eb4d9e199cbe9ade6
Zait <ayalaorez@gmail.com>
2020-07-17 11:40:13 -0500

    add sync_to_async

--- books/views.py
+++ books/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
+
+from asgiref.sync import sync_to_async
 
 from .models import Book
 
 
 class BookDetailView(DetailView):
     model = Book
+
+    def get_object(self, queryset=None):
+        return sync_to_async(super().get_object(queryset))
 
     async def __call__(self):
         return super().__call__(self)
